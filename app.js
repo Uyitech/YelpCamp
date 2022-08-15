16c67604474a0395ad6d314a7e31791cfa33002c
Uyitech <ewansihaevans@gmail.com>
2022-08-15 11:30:48 +0100

    add start script

--- app.js
+++ app.js
@@ -20,23 +20,11 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require('connect-mongo');
 
-// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-
-// mongoose.connect(dbUrl, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false
-// });
-
-
-
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useFindAndModify: false
 });
 
 
